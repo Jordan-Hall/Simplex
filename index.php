96155c63787281964bd053b29427cc67102a7c43
Jordan-Hall <nukezilla@hotmail.co.uk>
2012-08-07 11:49:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ include("include/config.php");
 // siteheader calls all files needed for the theme (top half which will be on every page)
 siteheader();
 
-// start of your page content (This is the dyanmic part of the website that changed depending on the page. please change the  "home" to which ever page you need to pull the file from.\\
+// start of your page content (This is the dyanmic part of the website that changed depending on the page. please change the  "home" to which ever page you need to pull the file from.)\\
 include_once(customcontent("home"));
 // end of your page content \\
 
