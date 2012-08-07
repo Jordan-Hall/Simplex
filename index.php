b03c1993a300b95a12c7d09b93aa921e9411dc85
Jordan-Hall <nukezilla@hotmail.co.uk>
2012-08-07 11:49:02 +0200

    Update index.php
    Edit the readability of the script. This will now explain what each section do. 

--- index.php
+++ index.php
@@ -1,13 +1,29 @@
 <?
+/*******************************************************************************
+Name: SimplexPHP
+Version: 0.1
+Website: http://github.com/Jordan-Hall/Simplex
+Author: Luno Solutions ltd <simplex@lunosolutions.com>
+Acknowledge: S.C. Chen (http://sourceforge.net/projects/simplehtmldom/)
+Contributions by:
+    Bennett Treptow (Upload class)
+Please leave this in tac. You may pass SimplexPHP on and add to simplex.
+However you must not claim this as your own
+*******************************************************************************/
 
+// This script will call functions needed to get the theme inserted
+
+// Leave this here. This controls SimplexPHP and is where the system is built.
 include("include/config.php");
 
+// siteheader calls all files needed for the theme (top half which will be on every page)
 siteheader();
 
-// start of your page content \\
+// start of your page content (This is the dyanmic part of the website that changed depending on the page. please change the  "home" to which ever page you need to pull the file from.\\
 include_once(customcontent("home"));
 // end of your page content \\
 
+// sitefooter calls all files needed for the theme (bottom which will be on every page)
 sitefooter();
 
 ?>
